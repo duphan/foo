86de169bf18ae6f7db546396f18dd623c876b7bd
Dung Phan <phandu@dungs-mbp.lan>
2019-01-05 21:35:35 -0500

    add all file from foo

---
+++ bar/config.js
@@ -0,0 +1,8 @@
+//const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/af799227-05d3-4a5b-ae27-8e378f5683b1/token";
+const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/bff73ede-fc45-49d2-81a6-b3eafd8b8704/token"; 
+//const instanceLocator = "v1:us1:af799227-05d3-4a5b-ae27-8e378f5683b1";
+const instanceLocator = "v1:us1:bff73ede-fc45-49d2-81a6-b3eafd8b8704";
+
+exports.tokenUrl = tokenUrl;
+exports.instanceLocator = instanceLocator;
+
